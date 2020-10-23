6e6052a89a364b551a3fffeb1cf6f4a721d5f213
SantiagoVillalba <67521266+SantiagoVillalba@users.noreply.github.com>
2020-10-22 22:32:45 -0300

    List

--- src/Library/CalculateCost.cs
+++ src/Library/CalculateCost.cs
@@ -1,21 +1,25 @@
+using System.Collections.Generic;
+
 namespace Full_GRASP_And_SOLID.Library
 {
     public class CalculateCost : ICost
     {
-        private Step step;
-        public CalculateCost(Step step)
+        private List<Step> steps;
+        public CalculateCost(List<Step> steps)
         {
-            this.step=step;
+            this.steps=steps;
         }
         public double GetProductionCost()
         {
             double cost = 0;   
+            foreach(Step step in steps)
+            {
+                //Costos de los Insumos
+                cost += step.Input.UnitCost * step.Quantity;
 
-            //Costos de los Insumos
-            cost += step.Input.UnitCost * step.Quantity;
-
-            //Costos del Equipamiento
-            cost += step.Equipment.HourlyCost * step.Time;
+                //Costos del Equipamiento
+                cost += step.Equipment.HourlyCost * step.Time;
+            }
             
             return cost;
         }
